e83968c32e138b58c71761f72496e4080820aa60
jun <526597019@qq.com>
2025-07-24 10:00:24 +0800

    function: 	1. 添加序列号管理插件 serialnumber 	2. 此功能尚未完善，只是首次编译通过

---
+++ utils/serialnumber/src/serialnumber.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main(int argc, char **argv)
+{
+	while(true)
+	{
+		sleep(1);
+		printf("Serial Number...\n");
+	}
+
+	return 0;
+}
